3fe49d7bd762fffde5699e4b3bb603218ae39216
Shilika0 <zseszti42@gmail.com>
2023-02-15 20:49:11 +0100

    minden jo uwu

--- script.js
+++ script.js
@@ -11,8 +11,15 @@ function CreatTheNewsPaper() {
     var div = document.createElement('div');
     div.setAttribute('class', 'fedo');
 
-    div.style.width = screen.width*0.4 + "px";
-    div.style.height = screen.width*0.4*(1024/722) + "px";
+    if (telon)
+    { //telo
+        div.style.width = screen.width*0.8 + "px";
+        div.style.height = screen.width*0.8*(1024/722) + "px";
+    }
+    else { //gep
+        div.style.width = screen.width*0.4 + "px";
+        div.style.height = screen.width*0.4*(1024/722) + "px";
+    }
 
     div.style.backgroundImage = 'url(1.png)'
     document.getElementById("ujsag").appendChild(div);
@@ -22,8 +29,15 @@ function CreatTheNewsPaper() {
         var div = document.createElement('div');
         div.setAttribute('class', 'lapok');
         
-        div.style.width = screen.width*0.7 + "px";
-        div.style.height = screen.width*0.7*(1024/1444) + "px";
+        if (telon)
+        { //telo
+            div.style.width = screen.width + "px";
+            div.style.height = screen.width*(1024/1444) + "px";
+        }
+        else { //gep
+            div.style.width = screen.width*0.7 + "px";
+            div.style.height = screen.width*0.7*(1024/1444) + "px";
+        }
 
         var backG = i + ".png"
         div.style.backgroundImage = 'url('+backG+')'
@@ -34,8 +48,15 @@ function CreatTheNewsPaper() {
     var div = document.createElement('div');
     div.setAttribute('class', 'fedo');
 
-    div.style.width = screen.width*0.4 + "px";
-    div.style.height = screen.width*0.4*(1024/722) + "px";
+    if (telon)
+    { //telo
+        div.style.width = screen.width*0.8 + "px";
+        div.style.height = screen.width*0.8*(1024/722) + "px";
+    }
+    else { //gep
+        div.style.width = screen.width*0.4 + "px";
+        div.style.height = screen.width*0.4*(1024/722) + "px";
+    }
 
     div.style.backgroundImage = 'url(17.png)'
     document.getElementById("ujsag").appendChild(div);
