c3249279fcd1bcd458e9ca978fdbfd7d617c3428
Shilika0 <zseszti42@gmail.com>
2023-02-15 20:35:36 +0100

    jo pages

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 function CreatTheNewsPaper() {
-    for (let i = 0; i < 17; i++) {
+
+    //FRONT PAGE
+    var div = document.createElement('div');
+        div.setAttribute('class', 'fedo');
+        div.style.backgroundImage = 'url(1.png)'
+        document.getElementById("ujsag").appendChild(div);
+
+    //EVERYTHING ELSE
+    for (let i = 2; i < 17; i++) {
         var div = document.createElement('div');
         div.setAttribute('class', 'lapok');
+        
+        div.style.width = screen.width*0.7 + "px";
+        div.style.height = screen.width*0.7*(1024/1444) + "px";
+        var backG = i + ".png"
+        div.style.backgroundImage = 'url('+backG+')'
         document.getElementById("ujsag").appendChild(div);
     }
+
+    //LAST PAGE
+    var div = document.createElement('div');
+    div.setAttribute('class', 'fedo');
+    div.style.backgroundImage = 'url(17.png)'
+    document.getElementById("ujsag").appendChild(div);
 }
 
 CreatTheNewsPaper()
